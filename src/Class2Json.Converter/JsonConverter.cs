113a576ed0dff65c40a7e4637858e0eb1b8a6ddb
vadimkholodilo <vadimkholodilo@gmail.com>
2024-12-07 17:31:07 +0300

    Create class to json converter using reflection

---
+++ src/Class2Json.Converter/JsonConverter.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Class2Json.Converter
+{
+    public static class JsonConverter
+    {
+        public static string ConvertClass(string sourceCode, bool useCamelCase = true)
+        {
+            if (string.IsNullOrWhiteSpace(sourceCode))
+                return string.Empty;
+
+            var compiledAssembly = CompileSourceCode(sourceCode);
+            var classTypes = compiledAssembly.GetTypes().Where(t => t.IsClass).ToList();
+
+            var jsonProperties = new Dictionary<string, object>();
+
+            foreach (var classType in classTypes)
+            {
+                var classInstance = Activator.CreateInstance(classType);
+                foreach (var property in classType.GetProperties())
+                {
+                    var jsonKey = useCamelCase ? ToCamelCase(property.Name) : property.Name;
+                    if (property.PropertyType.IsClass && property.PropertyType != typeof(string))
+                    {
+                        jsonProperties[jsonKey] = GetNestedClassProperties(property.PropertyType);
+                    }
+                    else
+                    {
+                        jsonProperties[jsonKey] = GetDefaultValue(property.PropertyType);
+                    }
+                }
+            }
+
+            return JsonSerializer.Serialize(jsonProperties);
+        }
+
+        private static Assembly CompileSourceCode(string sourceCode)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+            var references = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.Location))
+                .Select(a => MetadataReference.CreateFromFile(a.Location))
+                .Cast<MetadataReference>();
+
+            var compilation = CSharpCompilation.Create("DynamicAssembly")
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .AddReferences(references)
+                .AddSyntaxTrees(syntaxTree);
+
+            using var ms = new MemoryStream();
+            var result = compilation.Emit(ms);
+
+            if (!result.Success)
+            {
+                var errors = string.Join(Environment.NewLine, result.Diagnostics
+                    .Where(diagnostic => diagnostic.Severity == DiagnosticSeverity.Error)
+                    .Select(diagnostic => diagnostic.ToString()));
+                throw new InvalidOperationException($"Compilation failed: {errors}");
+            }
+
+            ms.Seek(0, SeekOrigin.Begin);
+            return Assembly.Load(ms.ToArray());
+        }
+
+        private static string ToCamelCase(string str)
+        {
+            if (string.IsNullOrEmpty(str) || !char.IsUpper(str[0]))
+                return str;
+
+            var chars = str.ToCharArray();
+            chars[0] = char.ToLower(chars[0]);
+            return new string(chars);
+        }
+
+        private static object GetDefaultValue(Type type)
+        {
+            if (type.IsValueType)
+            {
+                return Activator.CreateInstance(type);
+            }
+
+            return type == typeof(string) ? string.Empty : null;
+        }
+
+        private static Dictionary<string, object> GetNestedClassProperties(Type type)
+        {
+            var nestedProperties = type.GetProperties();
+            var nestedJsonProperties = new Dictionary<string, object>();
+
+            foreach (var property in nestedProperties)
+            {
+                var jsonKey = ToCamelCase(property.Name);
+                nestedJsonProperties[jsonKey] = GetDefaultValue(property.PropertyType);
+            }
+
+            return nestedJsonProperties;
+        }
+    }
+}
---
+++ test/Class2Json.Converter.Tests/JsonConverterTests.cs
@@ -0,0 +1,96 @@
+﻿namespace Class2Json.Converter.Tests;
+
+public class JsonConverterTests
+{
+    [Fact]
+    public void ConvertClass_ShouldReturnEmptyString_IfSourceCodeIsEmptyString()
+    {
+        var sourceCode = "";
+        var expectedJson = "";
+
+        var json = JsonConverter.ConvertClass(sourceCode);
+
+        Assert.Equal(expectedJson, json);
+    }
+
+    [Fact]
+    public void ConvertClass_ShouldReturnExpectedJson_IfSourceCodeIsValid()
+    {
+        var sourceCode = @"
+            public class Person
+            {
+                public string FirstName { get; set; }
+                public string LastName { get; set; }
+            }
+        ";
+        var expectedJson = "{\"firstName\":\"\",\"lastName\":\"\"}";
+
+        var json = JsonConverter.ConvertClass(sourceCode);
+
+        Assert.Equal(expectedJson, json);
+    }
+
+    [Fact]
+    public void ConvertClass_ShouldHandlePrimitiveTypes()
+    {
+        var sourceCode = @"
+            public class Sample
+            {
+                public int Age { get; set; }
+                public double Height { get; set; }
+                public bool IsActive { get; set; }
+                public string Name { get; set; }
+            }
+        ";
+        var expectedJson = "{\"age\":0,\"height\":0,\"isActive\":false,\"name\":\"\"}";
+
+        var json = JsonConverter.ConvertClass(sourceCode);
+
+        Assert.Equal(expectedJson, json);
+    }
+
+    [Fact]
+    public void ConvertClass_ShouldHandleNestedClasses()
+    {
+        var sourceCode = @"
+            public class Address
+            {
+                public string Street { get; set; }
+                public string City { get; set; }
+            }
+
+            public class Person
+            {
+                public string FirstName { get; set; }
+                public string LastName { get; set; }
+                public Address Address { get; set; }
+            }
+        ";
+        var expectedJson = "{\"firstName\":\"\",\"lastName\":\"\",\"address\":{\"street\":\"\",\"city\":\"\"}}";
+
+        var json = JsonConverter.ConvertClass(sourceCode);
+
+        Assert.Equal(expectedJson, json);
+    }
+
+    [Fact]
+    public void ConvertClass_ShouldHandleClassWithBaseClass()
+    {
+        var sourceCode = @"
+            public class BaseClass
+            {
+                public int Id { get; set; }
+            }
+
+            public class DerivedClass : BaseClass
+            {
+                public string Name { get; set; }
+            }
+        ";
+        var expectedJson = "{\"id\":0,\"name\":\"\"}";
+
+        var json = JsonConverter.ConvertClass(sourceCode);
+
+        Assert.Equal(expectedJson, json);
+    }
+}
